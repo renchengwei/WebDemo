2cf6e5637b263a9e788024ccb8c37f8bd0ccfcfe
任成威 <renchengwei@renchengweideMacBook-Pro.local>
2017-05-12 21:32:53 +0800

    优化页面效果

--- src/main/java/com/rencw/action/LoginAction.java
+++ src/main/java/com/rencw/action/LoginAction.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
